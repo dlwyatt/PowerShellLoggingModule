cf506b8b6d827584b947aeb8d085ddf6b74ba9e2
Dave Wyatt <dlwyatt@e-wyatt.org>
2013-08-04 00:43:57 -0400

    Added latest release dll to the Module folder, moved most of the code in PSLogging.psm1 inside a try block.
    Compiled DLL in the Module\PSLogging folder is for convenience of people
    who want to use the module but can't compile it themselves.

--- Module/PSLogging/PSLogging.psm1
+++ Module/PSLogging/PSLogging.psm1
@@ -1,4 +1,4 @@
-# Cmdlets and associated data types are defined in PowerShellLoggingModule.dll.  This script file just handles attaching and detaching the PSHostIOInterceptor object.
+# Cmdlets and associated data types are defined in PowerShellLoggingModule.dll.  This script file just handles attaching and detaching the HostIOInterceptor object.
 
 $dllPath = Join-Path -Path $MyInvocation.MyCommand.ScriptBlock.Module.ModuleBase -ChildPath PowerShellLoggingModule.dll
 
@@ -8,22 +8,23 @@ try {
     throw
 }
 
-# Attach the interceptor
-
-$flags = [System.Reflection.BindingFlags]([System.Reflection.BindingFlags]::Instance -bor [System.Reflection.BindingFlags]::NonPublic)
-
-$uiRef = $Host.GetType().GetField('internalUIRef', $flags).GetValue($Host)
-$ui = $uiRef.GetType().GetProperty('Value', $flags).GetValue($uiRef, $null)
-
-$externalUIField = $ui.GetType().GetField('externalUI', $flags)
-$originalUI = $externalUIField.GetValue($ui)
-
 try {
+    # Attach the interceptor
+
+    $flags = [System.Reflection.BindingFlags]([System.Reflection.BindingFlags]::Instance -bor [System.Reflection.BindingFlags]::NonPublic)
+
+    $uiRef = $Host.GetType().GetField('internalUIRef', $flags).GetValue($Host)
+    $ui = $uiRef.GetType().GetProperty('Value', $flags).GetValue($uiRef, $null)
+
+    $externalUIField = $ui.GetType().GetField('externalUI', $flags)
+    
+    $originalUI = $externalUIField.GetValue($ui)
+    
     $HostIOInterceptor = [PSLogging.HostIOInterceptor]::GetInterceptor()
     $HostIOInterceptor.HostUI = $originalUI
 
     $externalUIField.SetValue($ui, $HostIOInterceptor)
-
+    
     # Detach the interceptor when the module is removed.
     $MyInvocation.MyCommand.ScriptBlock.Module.OnRemove = {
         $script:externalUIField.SetValue($script:ui, $script:originalUI)
@@ -31,4 +32,3 @@ try {
 } catch {
     throw
 }
-
